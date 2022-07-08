2f99390f164a9e779c86b913cb3cd10150818593
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-07-08 15:15:05 -0300

    Navbar creation and styling

--- src/Components/Styles/Navbar.styles.js
+++ src/Components/Styles/Navbar.styles.js
@@ -2,9 +2,27 @@ import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
     color: white;
-    a{ color: blue;
-    font-size: 1rem;
-}
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 2rem;
+        a { color: blue;
+        font-size: 1rem;
+        text-decoration: none;
+        margin:2rem;
+        color:white;
+        transition: font-size 0.2s ;
+        
+    }
+    a:hover{
+        text-decoration: underline;
+        font-size: 20px;
+        
+        
+    }
+        li{
+            list-style:none;
+        }
 `
 
 
