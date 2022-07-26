acad28bf66ee4555c673f355dd2f6c52809053a2
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-07-26 10:24:31 -0300

    Modifying CardsData Img Routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <Router>
       <Navbar/>
         <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route path="" element={<Home/>}/>
         </Routes>
     </Router>
     </div>
